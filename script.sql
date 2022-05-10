52bfb59c44a38254fa62a65d5e71d7a5067fdb12
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-10 09:20:11 -0300

    apostila12-functions: bloco de codigo 2.7.1 terminado

--- script.sql
+++ script.sql
@@ -63,6 +63,38 @@ $$
 
 SELECT fn_executa ('fn_ehPar', 6);
 
+CREATE OR REPLACE FUNCTION fn_some (IN fn_funcao TEXT, VARIADIC elementos INT[])
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	elemento INT;
+	resultado BOOLEAN;
+	
+	-- 1, 2, 5, 6
+	-- 1, 1, 3
+BEGIN
+	FOREACH elemento IN ARRAY elementos LOOP
+		EXECUTE format('SELECT %s (%s)', fn_funcao, elemento) INTO resultado;
+		IF resultado = TRUE THEN
+			RETURN TRUE;
+		END IF;		
+	END LOOP;
+	RETURN FALSE;	
+END;
+$$
+
+DO $$
+DECLARE 
+	resultado BOOLEAN;
+BEGIN
+	SELECT fn_some ('fn_ehPar', 1, 2) INTO resultado;
+	RAISE NOTICE '%', resultado;
+	SELECT fn_some ('fn_ehPar', 1, 3, 5) INTO resultado;
+	RAISE NOTICE '%', resultado;
+END;
+$$
+
 	
 	
 	
