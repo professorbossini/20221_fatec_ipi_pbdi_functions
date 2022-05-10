99a13743d4b39660e567ab0e1cb814117e7587e8
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-10 10:11:29 -0300

    apostila12-functions: bloco de codigo 2.9.1 terminado

--- script.sql
+++ script.sql
@@ -122,7 +122,43 @@ BEGIN
 	RAISE NOTICE '%', resultado;
 END;
 $$
+-------------------------------------------------------------------------------------
+-- criação das tabelas
 
+
+-- DROP TABLE tb_conta;
+-- DROP TABLE tb_tipo_conta;
+-- DROP TABLE tb_item_pedido;
+-- DROP TABLE tb_pedido;
+-- DROP TABLE tb_cliente;
+
+CREATE TABLE tb_cliente (
+	cod_cliente SERIAL PRIMARY KEY,
+	nome VARCHAR(200) NOT NULL
+);
+
+INSERT INTO tb_cliente (nome) VALUES ('João Santos'), ('Maria Andrade');
+SELECT * FROM tb_cliente;
+
+CREATE TABLE tb_tipo_conta(
+	cod_tipo_conta SERIAL PRIMARY KEY,
+	descricao VARCHAR(200) NOT NULL
+);
+INSERT INTO tb_tipo_conta (descricao) VALUES ('Conta corrente'),('Conta poupança');
+SELECT * FROM tb_tipo_conta;
+
+CREATE TABLE tb_conta(
+	cod_conta SERIAL PRIMARY KEY,
+	status VARCHAR(200) NOT NULL DEFAULT 'aberta',
+	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	data_ultima_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	saldo NUMERIC(10, 2) NOT NULL CHECK (saldo >= 1000),
+	cod_cliente INT NOT NULL,
+	cod_tipo_conta INT NOT NULL,
+	CONSTRAINT fk_cliente FOREIGN KEY (cod_cliente) REFERENCES tb_cliente(cod_cliente),
+	CONSTRAINT fk_tipo_conta FOREIGN KEY (cod_tipo_conta) REFERENCES tb_tipo_conta (cod_tipo_conta)
+);
+SELECT * FROM tb_conta;
 
 	
 	
